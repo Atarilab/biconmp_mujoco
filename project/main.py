d754b4c01aa0da604769b772117a5211f009f6fc
Victor Dhedin <victor.dhedin@tum.de>
2024-09-19 13:32:19 +0000

    Update mj_pin_wrapper and code base accordingly. Add data recorder for MPC data.

--- project/main.py
+++ project/main.py
@@ -5,6 +5,7 @@ from mpc_controller.motions.cyclic.go2_trot import trot
 from mpc_controller.motions.cyclic.go2_jump import jump
 from mpc_controller.motions.cyclic.go2_bound import bound
 from mpc_controller.bicon_mpc import BiConMPC
+from mpc_controller.mpc_data_recorder import MPCDataRecorder
 
 from mj_pin_wrapper.sim_env.utils import RobotModelLoader
 from mj_pin_wrapper.mj_pin_robot import MJPinQuadRobotWrapper
@@ -30,18 +31,25 @@ if __name__ == "__main__":
     v_des, w_des = np.array([0.3, 0., 0.]), 0
     controller.set_command(v_des, w_des)
     # Set gait
-    controller.set_gait_params(jump)  # Choose between trot, jump and bound
+    controller.set_gait_params(trot)  # Choose between trot, jump and bound
+    
+    ###### Data recorder
+    data_recorder = MPCDataRecorder(robot, controller, "./data")
 
     ###### Simulator
-    simulator = Simulator(robot.mj, controller)
+    simulator = Simulator(robot.mj, controller, data_recorder)
     # Visualize contact locations
     visual_callback = (lambda viewer, step, q, v, data :
         desired_contact_locations_callback(viewer, step, q, v, data, controller))
     # Run simulation
-    sim_time = 10 #s
+    sim_time = 5 #s
     simulator.run(
         simulation_time=sim_time,
-        viewer=True,
+        use_viewer=True,
         real_time=False,
         visual_callback_fn=visual_callback,
+        force_duration=0.75,
+        force_intensity=10.
     )
+    
+    data_recorder.save()
